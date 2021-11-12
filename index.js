a08fd2d61ae663f9c587d1872479e70434d96e43
Joshua Patterson <jleegippies@gmail.com>
2021-11-12 08:37:05 -0600

    Set dark theme and better screen resolution

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ remoteMain.initialize();
 
 function createWindow () {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1280,
+        height: 720,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true,
